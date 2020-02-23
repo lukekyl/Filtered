71abcf94d1e984d3700ac3758bba036f7c16ad37
github username <github email address>
2020-02-22 17:24:00 -0700

    First Final Version

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
   root 'welcome#index'
-  # get 'signup' => 'users#new'
-  # post 'signup' => 'users#create'
-  # get 'login' => 'sessions#new'
-  # post 'login' => 'sessions#create'
-  # post 'logout' => 'sessions#destroy'
+
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
@@ -19,15 +15,12 @@ Rails.application.routes.draw do
 
   get "coffee/search" => "coffeees#index"
   match 'coffee/search', to: 'coffeees#index', via: 'post'
-  # match 'users/search/:by_roast'
-  # get 'users/:by_category', to: 'users#index'
-  # get 'users/:by_roast', to: 'users#index'
+
   
   resources :coffeees, path: 'coffee' do 
     resources :reviews
   end
 
   resources :brands
-    
 
 end
