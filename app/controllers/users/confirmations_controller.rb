a9e46aa32e4b0dbaf1fcb0fac0e93bdf52290180
Kyle Luke <luke.kylew@gmail.com>
2020-07-13 13:34:34 -0600

    Heroku Deployment setup

--- app/controllers/users/confirmations_controller.rb
+++ app/controllers/users/confirmations_controller.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
 class Users::ConfirmationsController < Devise::ConfirmationsController
-  GET /resource/confirmation/new
-  def new
-    super
-  end
+  # GET /resource/confirmation/new
+  # def new
+  #   super
+  # end
 
   POST /resource/confirmation
   def create
@@ -12,20 +12,20 @@ class Users::ConfirmationsController < Devise::ConfirmationsController
     redirect_to users_path
   end
 
-  GET /resource/confirmation?confirmation_token=abcdef
-  def show
-    super
-  end
+  # GET /resource/confirmation?confirmation_token=abcdef
+  # def show
+  #   super
+  # end
 
-  protected
+  # protected
 
-  The path used after resending confirmation instructions.
-  def after_resending_confirmation_instructions_path_for(resource_name)
-    super(resource_name)
-  end
+  # The path used after resending confirmation instructions.
+  # def after_resending_confirmation_instructions_path_for(resource_name)
+  #   super(resource_name)
+  # end
 
-  The path used after confirmation.
-  def after_confirmation_path_for(resource_name, resource)
-    super(resource_name, resource)
-  end
+  # The path used after confirmation.
+  # def after_confirmation_path_for(resource_name, resource)
+  #   super(resource_name, resource)
+  # end
 end
